9ce30c3cb4a1c279754ee49da3d1ad71afdfb393
senbeiasano <351515010@qq.com>
2024-09-10 20:13:02 +0800

    galois_field v0.1

---
+++ src/galois_field.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+class GaloisField:
+    # Galois Field GF(2^8) with modulo polynomial x^8 + x^4 + x^3 + x^2 + 1
+    def __init__(self):
+        self.polynomial = 0b100011101
+        self.gflog = [0] * 256
+        self.gfilog = [0] * 512
+        self._initialize_tables()
+
+    def _initialize_tables(self):
+        value = 1
+        for exp in range(255):
+            self.gfilog[exp] = value # x^0 = 1, value = 1, exp = 0
+            self.gflog[value] = exp
+            value <<= 1
+            if value & 0b100000000: # if value >= 256
+                value ^= self.polynomial
+    
+        # optimization: double the size of the tables to avoid modulo
+        for exp in range(255, 512):
+            self.gfilog[exp] = self.gfilog[exp - 255]
+
+    def add(self, a, b):
+        return a ^ b
+
+    def subtract(self, a, b):
+        return a ^ b
+
+    # deprecated
+    def inverse(self, a):
+        return self.gfilog[255 - self.gflog[a]] 
+    
+    def multiply(self, a, b):
+        if a == 0 or b == 0:
+            return 0
+        return self.gfilog[self.gflog[a] + self.gflog[b]] # a * b = x^m + x^n = x^(m + n)
+
+    def divide(self, a, b):
+        if a == 0:
+            return 0
+        if b == 0:
+            raise ZeroDivisionError("division by zero")
+        # return self.multiply(a, self.inverse(b))
+        return self.gfilog[255 + self.gflog[a] - self.gflog[b]] # a / b = x^m / x^n = x^(m - n) = x^(255 + m - n)
+
--- src/raid6.py
+++ src/raid6.py
@@ -14,49 +14,7 @@
 import numpy as np
 from copy import deepcopy
 from dataclasses import dataclass, field
-
-
-# Suppose we have a class GaloisField that implements arithmetic operations in GF(256)
-class GaloisField:
-    '''
-    This is a demo class for Galois Field arithmetic operations in GF(256)
-    '''
-    def __init__(self):
-        self.exp = np.zeros(512, dtype=np.uint8)
-        self.log = np.zeros(256, dtype=np.uint8)
-        self._initialize_tables()
-
-    def _initialize_tables(self):
-        x = 1
-        for i in range(255):
-            self.exp[i] = x
-            self.log[x] = i
-            x = x << 1
-            if x & 0x100:
-                x = x ^ 0x11b
-        for i in range(255, 512):
-            self.exp[i] = self.exp[i - 255]
-
-    def add(self, a, b):
-        return a ^ b
-
-    def subtract(self, a, b):
-        return a ^ b
-
-    def multiply(self, a, b):
-        if a == 0 or b == 0:
-            return 0
-        return self.exp[(self.log[a] + self.log[b]) % 255]
-
-    def divide(self, a, b):
-        if a == 0:
-            return 0
-        if b == 0:
-            raise ZeroDivisionError("division by zero")
-        return self.exp[self.log[a] + 255 - self.log[b]]
-
-    def inverse(self, a):
-        return self.exp[255 - self.log[a]]
+from galois_field import GaloisField
 
 # Define a class to simulate each disk in the RAID6 system
 class Disk:
