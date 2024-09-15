39a0030cd2818b4b58023323ec7f60ba39cddaf8
senbeiasano <351515010@qq.com>
2024-09-15 23:20:04 +0800

    fix a definition bug

--- src/clib/bindings.cpp
+++ src/clib/bindings.cpp
@@ -6,14 +6,6 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(galois_field, m) {
-    // py::class_<GaloisField>(m, "GaloisField")
-    //     .def(py::init<>())
-    //     .def("add", &GaloisField::add)
-    //     .def("subtract", &GaloisField::subtract)
-    //     .def("multiply", &GaloisField::multiply)
-    //     .def("divide", &GaloisField::divide)
-    //     .def("get_gfilog", &GaloisField::get_gfilog);
-    
     m.def("cal_parity", &cal_parity);
     m.def("cal_parity_8", &cal_parity_8);
     m.def("cal_parity_p", &cal_parity_p);
